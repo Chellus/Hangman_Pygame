76d9139f4b66970d1475f08a3b67534c362939db
Elias Riveros <elias4845@gmail.com>
2020-11-02 16:09:06 -0300

    Created window

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
+import pygame
 
+pygame.init()
+
+window_w = 800 # window width
+window_h = 600 # window height
+
+# create the window and the clock
+window = pygame.display.set_mode((window_w, window_h))
+clock =  pygame.time.Clock()
+
+# colors
+white = (255, 255, 255)
+
+# this function is called when the user closes the window
+def game_quit():
+    pygame.quit()
+    quit()
+
+# main game loop
+def game_loop():
+    running = True
+
+    while running:
+        window.fill(white)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                game_quit()
+
+        pygame.display.update()
+        clock.tick(30)
 
 if __name__ == "__main__":
-    print("PROJECT: Hangman_Pygame")
+    game_loop()
